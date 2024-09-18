5678ae7e2f5d9e46bb836e1ca7904d4a9edacad1
Direcub <bradleygalatas@gmail.com>
2024-09-18 19:33:23 -0400

    added frame limiting and setup shapes

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    time = pygame.time.Clock
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        time.tick(60)
+        dt = (time.tick() / 1000)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
